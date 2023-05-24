f864d5b8240333327e29442afd029b8530c6516e
shawon012 <shawon.shakib01@gmail.com>
2023-05-24 12:42:15 +0600

    category added

---
+++ src/Pages/Home/Category/Category.jsx
@@ -0,0 +1,54 @@
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import { FreeMode, Pagination } from "swiper";
+import img1 from '../../../assets/home/slide1.jpg'
+import img2 from '../../../assets/home/slide2.jpg'
+import img3 from '../../../assets/home/slide3.jpg'
+import img4 from '../../../assets/home/slide4.jpg'
+import img5 from '../../../assets/home/slide5.jpg'
+const Category = () => {
+    return (
+        <div className="mx-auto">
+            <Swiper
+
+                slidesPerView={4}
+                spaceBetween={67}
+                freeMode={true}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[FreeMode, Pagination]}
+                className="mySwiper mb-24"
+            >
+                <SwiperSlide>
+                    <img src={img1} alt="" />
+                    <h3 className="text-4xl uppercase text-center -mt-16 text-white">Salads</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={img2} alt="" />
+                    <h3 className="text-4xl uppercase text-center -mt-16 text-white">Pizzas</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={img3} alt="" />
+                    <h3 className="text-4xl uppercase text-center -mt-16 text-white">Soups</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={img4} alt="" />
+                    <h3 className="text-4xl uppercase text-center -mt-16 text-white">Deserts</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={img5} alt="" />
+                    <h3 className="text-4xl uppercase text-center -mt-16 text-white">Salads</h3>
+                </SwiperSlide>
+
+            </Swiper>
+        </div>
+
+
+    );
+};
+
+export default Category;
