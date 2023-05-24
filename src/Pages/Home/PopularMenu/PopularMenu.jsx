ffab324fe9af89ad6dafb4917b8e9821b525b0bc
shawon012 <shawon.shakib01@gmail.com>
2023-05-24 18:51:14 +0600

    button added

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -4,26 +4,30 @@ import MenuItem from '../../Shared/MenuItem/MenuItem';
 
 const PopularMenu = () => {
     const [menu, setMenu] = useState([])
-    useEffect( () =>{
+    useEffect(() => {
         fetch('menu.json')
-        .then(res => res.json())
-        .then(data => {
-            const populaItems = data.filter(item => item.category === 'popular')
-            setMenu(populaItems)})
+            .then(res => res.json())
+            .then(data => {
+                const populaItems = data.filter(item => item.category === 'popular')
+                setMenu(populaItems)
+            })
     }, [])
     return (
         <section className='mb-12'>
             <SectionTitle
-            heading={"from Our Menu"}
-            subHeading={"Popular Item"}
+                heading={"from Our Menu"}
+                subHeading={"Popular Item"}
             ></SectionTitle>
             <div className='grid  md:grid-cols-2 gap-10'>
                 {
-                    menu.map( item => <MenuItem
-                    key={item._id}
-                    item={item}
+                    menu.map(item => <MenuItem
+                        key={item._id}
+                        item={item}
                     ></MenuItem>)
                 }
+            </div>
+            <div className='flex justify-around'>
+                <button className="btn btn-outline border-0 border-b-4 mt-4">View full Menu</button>
             </div>
         </section>
     );
