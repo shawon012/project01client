b4d591ca336ba3c63afa88bf0487ac13e05e3c46
shawon012 <shawon.shakib01@gmail.com>
2023-05-27 10:31:30 +0600

    menu section updated with paralax

--- src/Pages/MenuPage/Menu/Menu.jsx
+++ src/Pages/MenuPage/Menu/Menu.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import image from '../../../assets/menu/banner3.jpg';
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 
 const Menu = () => {
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss| Menu</title>
-            </Helmet>
-            <h2>This is menu</h2>
+            </Helmet> 
+            <Cover image={image} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover image={image} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover image={image} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
