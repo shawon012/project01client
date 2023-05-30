ef7c7fbfe472c141c3f3724fd351c9962a387536
shawon012 <shawon.shakib01@gmail.com>
2023-05-30 10:47:19 +0600

    75-8 done and solved tailwind intelicessence problem

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -8,9 +8,9 @@ const Card = ({item}) => {
             <p className='bg-slate-900 text-white absolute right-0 mr-4 mt-4 px-4'>${price}</p>
             <div className="card-body flex flex-col items-center ">
                 <h2 className="card-title">{name}</h2>
-                <p>{recipe}</p>
+                <p className='text-center'>{recipe}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-outline border-0 border-b-4 mt-4 bg-slate-100">Add to Cart</button>
+                    <button className="border-orange-400 btn btn-outline border-0 border-b-4 mt-4 bg-slate-100">Add to Cart</button>
                 </div>
             </div>
         </div>
