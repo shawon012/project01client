4f08e1eb713b00f850d786722fdeed8e02381d21
shawon012 <shawon.shakib01@gmail.com>
2023-05-30 14:50:35 +0600

    76-4 done signup page and auth provider page setup done

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -6,7 +6,7 @@ import Navbar from '../Pages/Shared/Navbar/Navbar';
 const Main = () => {
     const location = useLocation();
     console.log(location)
-    const noHeaderfooter = location.pathname.includes('login')
+    const noHeaderfooter = location.pathname.includes('login') || location.pathname.includes('signup')
     return (
         <div>
             { noHeaderfooter || <Navbar></Navbar>}
--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,16 @@ import {
 } from "react-router-dom";
 import { router } from './Routes/Router';
 import { HelmetProvider } from 'react-helmet-async';
+import AuthProvider from './Providers/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <div className='max-w-screen-xl mx-auto'>
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <div className='max-w-screen-xl mx-auto'>
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
