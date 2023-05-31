cfe3422f471a31946802afed1e5434ba48186e67
shawon012 <shawon.shakib01@gmail.com>
2023-05-31 10:48:46 +0600

    77-2 done

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import { useContext } from 'react';
+import { AuthContext } from '../../Providers/AuthProvider';
+import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
 
-const Card = ({item}) => {
-    const {image, price, recipe, name} = item;
-    const handleAddToCart = item =>{
+const Card = ({ item }) => {
+    const { image, price, recipe, name } = item;
+    const { user } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const handleAddToCart = item => {
         console.log(item);
-        
+        if (user) {
+            fetch('http://localhost:5000/carts')
+                .then(res => res.json())
+                .then(data => {
+                    if (data.insertedId) {
+                        Swal.fire({
+                            title: 'Success!',
+                            showClass: {
+                                popup: 'animate__animated animate__fadeInDown'
+                            },
+                            hideClass: {
+                                popup: 'animate__animated animate__fadeOutUp'
+                            }
+                        })
+                    }
+                })
+        }
+        else {
+            Swal.fire({
+                title: 'Login!',
+                text: "Please Login To Add To Cart",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Login Now!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate('/login')
+                }
+            })
+        }
     }
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
